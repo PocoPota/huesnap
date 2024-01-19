781c78493ed0a23cca6a564a558acd3748500607
PocoPota <pocopota.program@gmail.com>
2024-01-19 23:51:53 +0900

    Create rounded corners

--- src/ImageEditor.jsx
+++ src/ImageEditor.jsx
@@ -4,17 +4,42 @@ const ImageEditor = ({ previewImage }) => {
   const [downloadLink, setDownloadLink] = useState(null);
   const canvasRef = useRef(null);
 
+  // Function to create rounded corners
+  const roundImage = (image, width, height, radius) => {
+    const roundedImageCanvas = document.createElement("canvas");
+    roundedImageCanvas.width = width;
+    roundedImageCanvas.height = height;
+    const roundedImageCtx = roundedImageCanvas.getContext("2d");
+
+    roundedImageCtx.drawImage(image, 0, 0, width, height);
+
+    roundedImageCtx.globalCompositeOperation = "destination-in";
+    roundedImageCtx.beginPath();
+    roundedImageCtx.moveTo(radius, 0);
+    roundedImageCtx.arcTo(width, 0, width, height, radius);
+    roundedImageCtx.arcTo(width, height, 0, height, radius);
+    roundedImageCtx.arcTo(0, height, 0, 0, radius);
+    roundedImageCtx.arcTo(0, 0, width, 0, radius);
+    roundedImageCtx.closePath();
+    roundedImageCtx.fill();
+
+    return roundedImageCanvas;
+  };
+
   const applyBackground = async () => {
     try {
       // canvas settings
       const canvas = canvasRef.current;
       const ctx = canvas.getContext("2d");
 
+      // Other settings
+      const spaceSize = 140;
+      const radius = 7;
+
       // Get given image
       const givenImage = new Image();
       givenImage.src = previewImage;
       await givenImage.decode();
-      const spaceSize = 140;
 
       // Set the canvas size
       canvasRef.current.width = givenImage.width + spaceSize * 2;
@@ -29,13 +54,21 @@ const ImageEditor = ({ previewImage }) => {
       // Draw background image on canvas
       ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
 
+      // Make rounded image
+      const roundedImage = roundImage(
+        givenImage,
+        givenImage.width,
+        givenImage.height,
+        radius
+      );
+
       // Definition of shadow
       ctx.shadowColor = "#555555";
       ctx.shadowBlur = 25;
 
       // Draw given image on canvas
       ctx.drawImage(
-        givenImage,
+        roundedImage,
         spaceSize,
         spaceSize,
         givenImage.width,
